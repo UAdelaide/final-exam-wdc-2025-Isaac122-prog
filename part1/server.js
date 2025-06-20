815d7d8a0efd65a505c4041b29b172aab6e8b072
Isaac <a1901982@student.adelaide.edu.au>
2025-06-20 02:25:56 +0000

    [automatic commit] 2025-06-20 02:25:56 UTC 139.218.226.37

--- part1/server.js
+++ part1/server.js
@@ -27,6 +27,7 @@ async function initialiseTestData() {
         INSERT INTO Dogs (owner_id, name, size) VALUES
         ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
         ((SELECT user_id FROM Users WHERE username = 'carol123;), 'Bella','small'),
-        ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Charlie'))`)
+        ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Charlie', 'large')
+        `)
     }
 }
