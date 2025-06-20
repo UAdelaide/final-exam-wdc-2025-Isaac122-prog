ec753f303e352fbf25713c4053a4a2a001580429
Isaac <a1901982@student.adelaide.edu.au>
2025-06-20 02:32:44 +0000

    [automatic commit] 2025-06-20 02:32:44 UTC 139.218.226.37

--- part1/server.js
+++ part1/server.js
@@ -57,5 +57,7 @@ app.get('/api/dogs', async (req, res) => {
         JOIN Users u ON d.owner_id = u.user_id
         `);
         res.json(results);
+    } catch (err) {
+        console.error('GET /api/dogs error:')
     }
 })
