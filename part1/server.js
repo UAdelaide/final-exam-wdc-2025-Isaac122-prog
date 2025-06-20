cb46861b7d4e2f71f6ef67b659ec7d6add55df61
Isaac <a1901982@student.adelaide.edu.au>
2025-06-20 02:35:06 +0000

    [automatic commit] 2025-06-20 02:35:06 UTC 139.218.226.37

--- part1/server.js
+++ part1/server.js
@@ -70,6 +70,10 @@ app.get('/api/walkrequests/open', async (req, res) => {
         wr.request_id,
         d.name AS dog_name,
         wr.requested_time,
-        wr.`)
+        wr.duration_minutes,
+        wr.location,
+        u.username AS owner_username
+        FROM WalkRequests wr
+        JOIN Dogs d ON wr.dog`)
     }
 })
