6e191d917f365ac24457d47e5e5d1bfda07f5133
Isaac <a1901982@student.adelaide.edu.au>
2025-06-21 04:45:39 +0000

    [automatic commit] 2025-06-21 04:45:39 UTC 139.218.226.37

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -70,6 +70,17 @@ router.post('/login', async (req, res) => {
   }
 });
 
+router.post('/logout', (req, res) => {
+  req.session.destroy(err => {
+    if (err) {
+      return res.status(500).json({ message: 'Logout failed' });
+    }
+    res.clearCookie('connect.sid');
+    res.json({ message: 'Logout successful' });
+  });
+});
+
+
 
 module.exports = router;
 
