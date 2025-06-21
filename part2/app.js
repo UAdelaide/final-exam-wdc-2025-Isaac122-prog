b5447c5f16a1bc2898c98b7f35cef414d115748c
Isaac <a1901982@student.adelaide.edu.au>
2025-06-21 05:43:05 +0000

    [automatic commit] 2025-06-21 05:43:05 UTC 139.218.226.37

--- part2/app.js
+++ part2/app.js
@@ -23,6 +23,7 @@ const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes); // or wherever this route is added
 
 
 // Export the app instead of listening here
