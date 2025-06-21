4e4dc45bfed56e0e0fa516416e25b881ffbf5fb9
Isaac <a1901982@student.adelaide.edu.au>
2025-06-21 05:40:43 +0000

    [automatic commit] 2025-06-21 05:40:43 UTC 139.218.226.37

---
+++ part2/Dogs.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Dogs (
+  dog_id INT AUTO_INCREMENT PRIMARY KEY,
+  owner_id INT NOT NULL,
+  name VARCHAR(100),
+  size VARCHAR(50),
+  -- other dog fields
+  FOREIGN KEY (owner_id) REFERENCES Users(user_id)
+);
