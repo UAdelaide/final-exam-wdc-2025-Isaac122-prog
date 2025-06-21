a5f11038de8428327cd16be19aea2dd408fce6d6
Isaac <a1901982@student.adelaide.edu.au>
2025-06-21 05:55:05 +0000

    [automatic commit] 2025-06-21 05:55:05 UTC 139.218.226.37

--- part2/Dogs.sql
+++ part2/Dogs.sql
@@ -5,3 +5,20 @@ CREATE TABLE Dogs (
   size VARCHAR(50),
   FOREIGN KEY (owner_id) REFERENCES Users(user_id)
 );
+
+
+INSERT INTO Users (username, email, password_hash, role) VALUES
+('ownerJane', 'jane@example.com', 'password123', 'owner'),
+('walkerMike', 'mike@example.com', 'password456', 'walker'),
+('ownerMax','max@example.com','password789', 'owner'),
+('walkerThomas','thomas@example.com','password234', 'walker');
+
+
++--------+----------+-------+--------+---------------------+
+| dog_id | owner_id | name  | size   | created_at          |
++--------+----------+-------+--------+---------------------+
+|      1 |        1 | Buddy | Medium | 2025-06-21 05:47:31 |
+|      2 |        1 | Lucy  | Small  | 2025-06-21 05:47:31 |
+|      3 |        3 | David | Large  | 2025-06-21 05:47:31 |
+|      4 |        3 | Emma  | Small  | 2025-06-21 05:47:31 |
++--------+----------+-------+--------+---------------------+
