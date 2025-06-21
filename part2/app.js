3b2559b45129e812a75da23f854fb536fe49747d
Isaac <a1901982@student.adelaide.edu.au>
2025-06-21 04:09:27 +0000

    [automatic commit] 2025-06-21 04:09:27 UTC 139.218.226.37

--- part2/app.js
+++ part2/app.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const path = require('path');
-require('dotenv').config();
+// const express = require('express');
+// const path = require('path');
+// require('dotenv').config();
 
-const app = express();
+// const app = express();
 
-// Middleware
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
+// // Middleware
+// app.use(express.json());
+// app.use(express.static(path.join(__dirname, '/public')));
 
-// Routes
-const walkRoutes = require('./routes/walkRoutes');
-const userRoutes = require('./routes/userRoutes');
+// // Routes
+// const walkRoutes = require('./routes/walkRoutes');
+// const userRoutes = require('./routes/userRoutes');
 
-app.use('/api/walks', walkRoutes);
-app.use('/api/users', userRoutes);
+// app.use('/api/walks', walkRoutes);
+// app.use('/api/users', userRoutes);
 
-// Export the app instead of listening here
-module.exports = app;
+// // Export the app instead of listening here
+// module.exports = app;
 
 
 
 const express = require('express');
 const router = express.Router();
 const mysql = require('mysql2/promise');
-const bcrypt = require('bcrypt'); // Optional if you use real hashing
+const bcrypt = require('bcrypt');
 
 // Configure your MySQL database
 const dbConfig = {
